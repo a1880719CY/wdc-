646a27baf9ccce3cf0ea475c3292e6bb0dcf6cb1
ChrisYu <140379272+a1880719CY@users.noreply.github.com>
2026-04-30 15:15:01 +0930

    Add files via upload

---
+++ 24S1_WDC_prac5-a1880719CY/query3-5.sql
@@ -0,0 +1,3 @@
+SELECT Students.given_name, Students.family_name, Enrolments.mark
+FROM ((Enrolments INNER JOIN Students ON Enrolments.student_id = Students.student_id)
+     INNER JOIN Subjects ON Enrolments.subject_code = Subjects.subject_code) WHERE Subjects.subject = '	Computer Systems' AND Enrolments.mark < 50;
